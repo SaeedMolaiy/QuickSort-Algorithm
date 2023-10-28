1bad15bb37063507b61a51350d9962414082eec9
Saeed Molaee <saeedmolaiy.dev@gmail.com>
2023-10-28 13:04:29 +0330

    Quick Sort Algorithm

---
+++ QuickSort/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = { 16, 4, 17, 6, 7, 3, 1, 15 };
+
+Console.WriteLine("Original Array:");
+PrintArray(arr);
+
+QuickSort.QuickSort.QuickSortAlgorithm(arr, 0, arr.Length - 1);
+
+Console.WriteLine("\nSorted Array:");
+PrintArray(arr);
+  
+Console.ReadKey();
+
+static void PrintArray(int[] arr)
+{
+    foreach (var num in arr)
+    {
+        Console.Write(num + " ");
+    }
+
+    Console.WriteLine();
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,39 @@
+﻿namespace QuickSort;
+
+internal class QuickSort
+{
+    public static void QuickSortAlgorithm(int[] array, int left, int right)
+    {
+        if (left >= right)
+        {
+            return;
+        }
+
+        var pivotIndex = Partition(array, left, right);
+
+        QuickSortAlgorithm(array, left, pivotIndex - 1);
+        QuickSortAlgorithm(array, pivotIndex + 1, right);
+    }
+
+    public static int Partition(int[] array, int left, int right)
+    {
+        var pivot = array[right];
+        var i = left - 1;
+
+        for (var j = left; j < right; j++)
+        {
+            if (array[j] >= pivot)
+            {
+                continue;
+            }
+
+            i++;
+
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+
+        (array[i + 1], array[right]) = (array[right], array[i + 1]);
+
+        return i + 1;
+    }
+}
